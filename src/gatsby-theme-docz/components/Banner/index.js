6c7de898696413d072d25314ab6d3a2784900955
Chris Tsang <tyt2y7@gmail.com>
2020-10-24 01:36:55 +0800

    Update

--- src/gatsby-theme-docz/components/Banner/index.js
+++ src/gatsby-theme-docz/components/Banner/index.js
@@ -5,6 +5,7 @@ export const Banner = () => {
   const [colorMode, setColorMode] = useColorMode()
   return (
     <img
+      style={{maxHeight: 72 + 'vh'}}
       src={colorMode === 'light' ? 'public/visioncortex-logo-square.svg' : 'public/visioncortex-logo-square-light.svg'}
     />
   )
