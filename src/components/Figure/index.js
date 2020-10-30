06935b0bc685fc65ea012c3cafb1d7e204fa7845
Chris Tsang <tyt2y7@gmail.com>
2020-10-31 01:08:54 +0800

    Editor vtracer docs

--- src/components/Figure/index.js
+++ src/components/Figure/index.js
@@ -3,12 +3,17 @@ import { jsx } from 'theme-ui'
 
 export const Figure = (props) => {
   let imgSrc = props.src;
-  let descText = (props.text)? props.text : '';
+  let descText = props.text || '';
+  let height = props.height || null;
   return (
   <div>
     <img
       alt={descText}
-      style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: 100 + '%'}}
+      style={{
+        display: 'block',
+        marginLeft: 'auto', marginRight: 'auto',
+        maxWidth: 100 + '%', height
+      }}
       src={'/public/' + imgSrc}
     />
     <h6 style={{textAlign: 'center'}}>
